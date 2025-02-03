64db6955fdabfaf80c7767f075abfb390804f9f5
Marsaielda <ida.altenstedt@gmail.com>
2025-02-03 12:53:54 +0100

    added file structure
    where we should work on the game engine pieces

---
+++ ConsoleApp1/Shard/GoD's Work/Tiles Libary/Tiles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.Shard.GoD_s_Work.Tiles_Libary
+{
+    internal class Tiles
+    {
+    }
+}
