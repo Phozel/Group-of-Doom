36e9d9313f7b9e8c3191d08e9a83167b68f20290
Marsaielda <ida.altenstedt@gmail.com>
2025-02-07 16:39:35 +0100

    moved TileFactory into game
    Moved everything but abstract Tile and Tags Enum into game.

--- ConsoleApp1/Shard/GoDsWork/TilesLibrary/Tile.cs
+++ ConsoleApp1/Shard/GoDsWork/TilesLibrary/Tile.cs
@@ -8,36 +8,7 @@ using System.Threading.Tasks;
 using System.Transactions;
 
 namespace Shard.Shard.GoD_s_Work.Tiles_Libary
-{
-    public class TileFactory
-    {
-        private static TileFactory tileFactory;
-
-        public static TileFactory getInstance()
-        {
-            if (tileFactory == null)
-            {
-                tileFactory = new TileFactory();
-            }
-            return tileFactory;
-        }
-        private TileFactory() { }
-        //public static TileFactory getInstance(Tile tile) { return tileFactory; }
-        //public static TileFactory getTileFactory() { return tileFactory; }
-
-        public void setTileWidth(float width)
-        {
-            Tile.width = width;
-        }
-        public Tile getGroundTile() {  return BasicTile.groundTile(); }
-        public Tile getWallTile() { return BasicTile.wallTile(); }
-        public Tile getTrap() { return new TrapTile(); }
-        public Tile getUnconnectedDoor() { return new DoorTile(); }
-        public Tile getConnectedDoor(Tile exit) { return new DoorTile(exit); }
-        public void connectDoor(Tile unconnectedDoor, Tile exit) { unconnectedDoor.connectTile(exit); }
-
-    }
-        
+{   
     public abstract class Tile
     {
         internal static float width = 10;
@@ -59,34 +30,6 @@ namespace Shard.Shard.GoD_s_Work.Tiles_Libary
         wall,
         trap,
         unconnectedDoor, door,
-    }
-
-    internal class BasicTile : Tile
-    {
-        internal static Tile groundTile() { return new BasicTile(Tag.ground); }
-        internal static Tile wallTile() { return new BasicTile(Tag.wall); }
-        private BasicTile(Tag tag) : base(tag) { }
-    }
-    internal class TrapTile : Tile
-    {
-        internal TrapTile() : base(Tag.trap) { }
-
-        override public void interaction() { }
-    }
-    internal class DoorTile : Tile
-    {
-        private Tile exit;
-        internal DoorTile() : base(Tag.unconnectedDoor) { }
-        internal DoorTile(Tile exit) : base(Tag.door) { this.exit = exit; }
-
-        /*internal void connectDoor(Tile unconnectedDoor, Tile exit)
-        {
-            this.exit = exit;
-            unconnectedDoor.tag = Tag.door;
-        }*/
-
-        override public void interaction() { }
-        override internal void connectTile(Tile exit) { this.exit = exit; this.tag = Tag.door; }
     }
 
 }
