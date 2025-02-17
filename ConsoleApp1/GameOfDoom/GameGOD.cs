9e89c101909de6e0463dc89933fb923b8f0b23bf
RebeckaWillig <willigr@student.chalmers.se>
2025-02-17 13:58:04 +0100

    It has begun ...

---
+++ ConsoleApp1/GameOfDoom/GameGOD.cs
@@ -0,0 +1,41 @@
+﻿using SDL2;
+using Shard.GameOfDoom;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Shard
+{
+    class GameGOD : Game, InputListener
+    {
+        
+
+
+        public override bool isRunning()
+        {
+            return true;
+        }
+        public override void update()
+        {
+
+        }
+
+        public void createObjects()
+        {
+
+        }
+
+        public override void initialize()
+        {
+            Bootstrap.getInput().addListener(this);
+
+            
+        }
+
+        public void handleInput(InputEvent inp, string eventType)
+        {
+
+
+        }
+    }
+}
