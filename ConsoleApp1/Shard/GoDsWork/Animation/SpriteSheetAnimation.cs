368dd3c8cc7cd0c97345d28d6bea9e88a2eb3b3b
Phozel <adamwi@student.chalmers.se>
2025-02-19 13:51:09 +0100

    Added some stuff to animations

---
+++ ConsoleApp1/Shard/GoDsWork/Animation/SpriteSheetAnimation.cs
@@ -0,0 +1,34 @@
+﻿using SDL2;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.Shard.GoDsWork.Animation
+{
+    class SpriteSheetAnimation(GameObject gameObject, List<string> spriteSheets)
+    {   
+        private List<string> _spriteSheets = spriteSheets;
+        private GameObject _gameObject = gameObject;
+        private int _currentSpriteSheet = 0;
+        
+        public void changeSprite()
+        {
+
+        }
+
+        public void swapSpriteSheet(int sheetIndex)
+        {
+            if (sheetIndex < _spriteSheets.Count)
+            {
+                _currentSpriteSheet = sheetIndex;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
