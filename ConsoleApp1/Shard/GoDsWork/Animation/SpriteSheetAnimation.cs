83ae4c249ff083b07b1bcdaf23091ecab87187a7
Phozel <thekoolstar@gmail.com>
2025-02-24 10:25:46 +0100

    SpriteSheetAnimations works
    Animations through spritesheets works now. It should work with all rectangular shaped sprites as long as individual sprite height and width is known.

--- ConsoleApp1/Shard/GoDsWork/Animation/SpriteSheetAnimation.cs
+++ ConsoleApp1/Shard/GoDsWork/Animation/SpriteSheetAnimation.cs
@@ -10,31 +10,44 @@ using System.Threading.Tasks;
 
 namespace Shard.Shard.GoDsWork.Animation
 {
-    class SpriteSheetAnimation(GameObject gameObject, string spriteSheet, int spriteWidth, int spriteHeight, int rows, int columns)
+
+    /*
+     * Class to handle all SpriteSheet animations. Can be used to select sprites for other reasons as well.
+     */
+    class SpriteSheetAnimation(GameObject gameObject, string spriteSheet, float spriteWidth, float spriteHeight, float rows, float columns)
     {   
         private string _spriteSheet = spriteSheet;
         private GameObject _gameObject = gameObject;
 
-        private int _spriteWidth = spriteWidth;
-        private int _spriteHeight = spriteHeight;
-        private int _rows = rows;
-        private int _columns = columns;
+        private float _spriteWidth = spriteWidth;
+        private float _spriteHeight = spriteHeight;
+        private float _rows = rows;
+        private float _columns = columns;
 
-        private Rectangle _spriteRect = new Rectangle(0, 0, spriteWidth, spriteHeight);
+        // Calculate how much space each sprite occupies in the sheet
+        private float _spriteWidthPercentage = spriteWidth / (float)(spriteWidth * columns);
+        private float _spriteHeightPercentage = spriteHeight / (float)(spriteHeight * rows);
 
+        // Default position of sprite in sheet
+        float SpritePosX = 0;
+        float SpritePosY = 0;
+
+        /*
+         * Function to change what Sprite in the SpriteSheet is being used starting from row & col 0
+         */
         public void changeSprite(int row, int col)
         {
+            // Calculates sprite position in sheet
+            SpritePosX = col * _spriteWidth;
+            SpritePosY = row * _spriteHeight;
 
-            _spriteRect.X = col * _spriteWidth;
-            _spriteRect.Y = row * _spriteHeight;
+            // Scales sprite appropriately
+            _gameObject.Transform.Scalex = _spriteWidthPercentage;
+            _gameObject.Transform.Scaley = _spriteHeightPercentage;
 
-            _gameObject.Transform.Scalex = 0.33f;
-            _gameObject.Transform.Scaley = 0.33f;
-
-            _gameObject.Transform.StartX = _spriteRect.X;
-            _gameObject.Transform.StartY = _spriteRect.Y;
-
-            //_gameObject.Transform.transformXYPosInImage(_spriteRect.X, _spriteRect.Y);
+            // Picks out correct sprite based on position
+            _gameObject.Transform.StartX = SpritePosX;
+            _gameObject.Transform.StartY = SpritePosY;
             
             _gameObject.Transform.SpritePath = Bootstrap.getAssetManager().getAssetPath(_spriteSheet);
             
