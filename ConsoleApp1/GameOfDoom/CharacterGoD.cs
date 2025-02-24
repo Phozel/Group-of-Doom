09795b8ffbe19c98706fbb21c3ea4c239309c742
Phozel <adamwi@student.chalmers.se>
2025-02-24 15:55:26 +0100

    Update CharacterGoD.cs

--- ConsoleApp1/GameOfDoom/CharacterGoD.cs
+++ ConsoleApp1/GameOfDoom/CharacterGoD.cs
@@ -11,17 +11,22 @@ namespace Shard.GameOfDoom
         private string _name { get; set; }
         private float _maxHealth { get; }
         private float _health { get; set; }
+        private int _firePower { get; set; }
         private float _armour { get; set; }
         //private List<Item> _inventory { get; set; }
         private float _movementSpeed { get; set; }
+        private string _imagePath { get; set; }
 
         public CharacterGoD() {
             _name = "God";
             _maxHealth = 100;
             _health = 100;
+            _firePower = 10;
             _armour = 0;
             _movementSpeed = (float)(100 * Bootstrap.getDeltaTime());
             //_inventory = new List<Item>();
+
+            _imagePath = "God.png";
         }
 
 
