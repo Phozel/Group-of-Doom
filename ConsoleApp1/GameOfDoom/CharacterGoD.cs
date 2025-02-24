a7549d360661adfda420586b479304e288201ea1
Phozel <adamwi@student.chalmers.se>
2025-02-24 15:47:46 +0100

    Created initial class

---
+++ ConsoleApp1/GameOfDoom/CharacterGoD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.GameOfDoom
+{
+    internal class CharacterGoD
+    {
+        private string _name { get; set; }
+        private float _maxHealth { get; }
+        private float _health { get; set; }
+        private float _armour { get; set; }
+        //private List<Item> _inventory { get; set; }
+        private float _movementSpeed { get; set; }
+
+        public CharacterGoD() {
+            _name = "God";
+            _maxHealth = 100;
+            _health = 100;
+            _armour = 0;
+            _movementSpeed = (float)(100 * Bootstrap.getDeltaTime());
+            //_inventory = new List<Item>();
+        }
+
+
+
+    }
+}
