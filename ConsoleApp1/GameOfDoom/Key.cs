700737f98944556c23031deb0540304895f52886
RebeckaWillig <willigr@student.chalmers.se>
2025-03-05 12:56:39 +0100

    Item and Key
    Added an abstract class Item which can be derives to create collectible item classes.
    Added item class Key

---
+++ ConsoleApp1/GameOfDoom/Key.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.GameOfDoom
+{
+    class Key : Item
+    {
+        public Key(int posx, int posy) : base("key", "Key.png", 64, 64, posx, posy)
+        {
+        }
+
+    }
+}
