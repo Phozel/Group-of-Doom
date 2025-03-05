8524ffda0b11940168bb8dca2f550c3676c79171
RebeckaWillig <willigr@student.chalmers.se>
2025-03-05 14:54:00 +0100

    HealthPack image

--- ConsoleApp1/GameOfDoom/GameGOD.cs
+++ ConsoleApp1/GameOfDoom/GameGOD.cs
@@ -42,8 +42,8 @@ namespace Shard
 
             Key key = new Key(300, 200);
             //Weapon gun = new Weapon(64, 128);
-            //Armor armor = new Armor(64, 192);
-            //HealthPack helth = new HealthPack(64, 256);
+            Armor armor = new Armor(64, 192);
+            HealthPack helth = new HealthPack(64, 256);
 
             Bootstrap.getSound().playMusic("examplemusic.wav", SDL.SDL_MIX_MAXVOLUME);
             (float, float) t = world.getAcceptibeSpawnPosition();
